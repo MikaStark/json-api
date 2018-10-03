046a23272a54b4f18ef16e59b0717a56f0fabdcf
Mickael BOIDIN <mboidin@digitalvirgo.com>
2018-10-03 18:13:35 +0200

    add langs params, better builder for query params

--- projects/json-api/src/lib/interfaces/parameters.ts
+++ projects/json-api/src/lib/interfaces/parameters.ts
@@ -1,4 +1,5 @@
 export interface Parameters {
+  langs?: string[];
   include?: string[];
   fields?: {[name: string]: string[]};
   sort?: string[];
--- projects/json-api/src/lib/json-api-parameters.service.ts
+++ projects/json-api/src/lib/json-api-parameters.service.ts
@@ -6,36 +6,28 @@ import { Parameters } from './interfaces/parameters';
   providedIn: 'root'
 })
 export class JsonApiParametersService {
-  private include(params: HttpParams, value: string[]): HttpParams {
-    return params.set('include', value.join(','));
-  }
-
-  private fields(params: HttpParams, values: {[name: string]: string[]}): HttpParams {
+  private listCollection(params: HttpParams, name: string, values: {[name: string]: string[]}): HttpParams {
     let newParams = params;
-    for (const name in values) {
-      if (values[name]) {
-        newParams = newParams.set(`fields[${name}]`, values[name].join(','));
+    for (const key in values) {
+      if (values[key]) {
+        newParams = newParams.set(`${name}[${key}]`, values[key].join(','));
       }
     }
     return newParams;
   }
 
-  private sort(params: HttpParams, value: string[]): HttpParams {
-    return params.set('sort', value.join(','));
-  }
-
-  private page(params: HttpParams, values: {[name: string]: number}): HttpParams {
+  private collection(params: HttpParams, name: string, values: {[name: string]: number}): HttpParams {
     let newParams = params;
-    for (const name in values) {
-      if (values[name]) {
-        newParams = newParams.set(`page[${name}]`, values[name].toString());
+    for (const key in values) {
+      if (values[key]) {
+        newParams = newParams.set(`${name}[${key}]`, values[key].toString());
       }
     }
     return newParams;
   }
 
-  private filter(params: HttpParams, value: string[]): HttpParams {
-    return params.set('filter', value.join(','));
+  private list(params: HttpParams, name: string, value: string[]): HttpParams {
+    return params.set(name, value.join(','));
   }
 
   httpParams(value: Parameters) {
@@ -43,20 +35,23 @@ export class JsonApiParametersService {
     if (!value) {
       return params;
     }
+    if (value.langs) {
+      params = this.list(params, 'langs', value.langs);
+    }
     if (value.include) {
-      params = this.include(params, value.include);
+      params = this.list(params, 'include', value.include);
     }
     if (value.fields) {
-      params = this.fields(params, value.fields);
+      params = this.listCollection(params, 'fields', value.fields);
     }
     if (value.sort) {
-      params = this.sort(params, value.sort);
+      params = this.list(params, 'sort', value.sort);
     }
     if (value.page) {
-      params = this.page(params, value.page);
+      params = this.collection(params, 'page', value.page);
     }
     if (value.filter) {
-      params = this.filter(params, value.filter);
+      params = this.list(params, 'filter', value.filter);
     }
     return params;
   }
