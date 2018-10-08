06d75796e336b67834991aca52f0839e49f45313
Mickael BOIDIN <mboidin@digitalvirgo.com>
2018-10-08 15:35:35 +0200

    add missing document-error

--- projects/json-api/src/lib/classes/index.ts
+++ projects/json-api/src/lib/classes/index.ts
@@ -2,6 +2,7 @@ export * from './document-resources';
 export * from './document-resource';
 export * from './document-identifier';
 export * from './document-identifiers';
+export * from './document-error';
 export * from './document';
 export * from './resource';
 export * from './service';
