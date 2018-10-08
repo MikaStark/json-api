504238385ac07d5ff9541cfcfe167bf5b38f2341
Mickael BOIDIN <mboidin@digitalvirgo.com>
2018-10-08 10:06:58 +0200

    remove useless code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,97 +1,10 @@
-import { Component, Injectable, OnInit } from '@angular/core';
-import { Resource, Service, Relationships, Relationship, JsonApiRegisterService, JsonApiFactoryService } from 'json-api';
-import { mergeMap, tap } from 'rxjs/operators';
-
-class Country extends Resource {
-  attributes: {
-    name?: {[locale: string]: string}
-  };
-  relationships: {
-    currencies: Relationships<Currency>,
-    taxes: Relationships<Tax>
-  };
-}
-
-class Currency extends Resource {
-  attributes: {
-    label: {[locale: string]: string}
-  };
-
-  relationships: {
-    country: Relationship<Country>
-  };
-}
-
-class Tax extends Resource {
-  attributes: {
-    label: {[locale: string]: string};
-    validity: string;
-  };
-  relationships: {
-    taxescategory: Relationship<TaxesCategory>;
-    country: Relationship<Country>;
-  };
-}
-
-class TaxesCategory extends Resource {
-  attributes: {
-    label: {[locale: string]: string}
-  };
-}
-
-class Firm extends Resource {
-  relationships: {
-    parentfirm: Relationship<Firm>
-  };
-}
-
-@Injectable({
-  providedIn: 'root',
-  useFactory: (factory: JsonApiFactoryService) => factory.service('countries', Country),
-  deps: [JsonApiFactoryService]
-})
-class CountriesService extends Service<Country> { }
-
-@Injectable({
-  providedIn: 'root',
-  useFactory: (factory: JsonApiFactoryService) => factory.service('firms', Firm),
-  deps: [JsonApiFactoryService]
-})
-class FirmsServices extends Service<Firm> { }
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'api-project';
-
-  constructor(
-    private countries: CountriesService,
-    private firms: FirmsServices,
-    register: JsonApiRegisterService
-  ) {
-    register.setMany({
-      firms: Firm,
-      countries: Country,
-      taxes: Tax,
-      taxes_categories: TaxesCategory,
-      currencies: Currency
-    });
-  }
-
-  ngOnInit() {
-    this.countries.all({
-      include: ['currencies', 'taxes', 'taxes.taxescategory']
-    }).subscribe(console.log);
-    this.countries.find('FR', {
-      include: ['currencies', 'taxes', 'taxes.taxescategory']
-    }).subscribe(console.log);
-    this.countries.find('FR', {
-      include: ['currencies'] }).pipe(
-      tap(console.log),
-      mergeMap(doc => doc.data.getRelationships('currencies'))
-    ).subscribe(console.log);
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { JsonApiModule, JSON_API_URL } from 'json-api';
-import { AuthorizationService } from './authorization.service';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    JsonApiModule
-  ],
-  providers: [
-    {
-      provide: JSON_API_URL,
-      useValue: 'http://pp.dev-all-docker1.infradve.fr/api'
-    },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthorizationService,
-      multi: true
-    }
+    BrowserModule
   ],
   bootstrap: [AppComponent]
 })
